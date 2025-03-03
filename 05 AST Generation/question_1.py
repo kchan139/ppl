b7ca7d1f5baec7165d8fb1347a63bcfbaa96db9d
Khoa Tran <khoatrandd.139@gmail.com>
2025-03-03 17:36:26 +0700

    style: reformat code

--- 05 AST Generation/question_1.py
+++ 05 AST Generation/question_1.py
@@ -36,25 +36,20 @@ class TerminalCount(MPVisitor):
     def visitProgram(self,ctx:MPParser.ProgramContext):
         return 1 + self.visit(ctx.vardecls())
 
-
     def visitVardecls(self,ctx:MPParser.VardeclsContext):
         return self.visit(ctx.vardecl()) + self.visit(ctx.vardecltail())
-    
 
     def visitVardecltail(self,ctx:MPParser.VardecltailContext): 
         if ctx.vardecl(): 
             return self.visit(ctx.vardecl()) + self.visit(ctx.vardecltail())
         return 0
 
-
     def visitVardecl(self,ctx:MPParser.VardeclContext): 
         return 1 + self.visit(ctx.mptype()) + self.visit(ctx.ids())
-
 
     def visitMptype(self,ctx:MPParser.MptypeContext):
         return 1
     
-
     def visitIds(self,ctx:MPParser.IdsContext):
         if ctx.ids(): 
             return 2 + self.visit(ctx.ids())
